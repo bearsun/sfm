60151b8804e3e19c820ca212eec737e26fd8ceeb
Liwei Sun <sunliwei.china@gmail.com>
2015-05-09 10:02:15 -0400

    ready for debug in the lab

--- sfm_demo.m
+++ sfm_demo.m
@@ -13,7 +13,18 @@ Priority(1);
 sid = [];
 sid = input('identifier for this session?');
 
-% Parameters
+% Run / Trial Parameters
+nRuns = 10;
+secsperrun = 240;
+
+% key
+kleft = KbName('Left');
+kright = KbName('Right');
+kesc = KbName('Escape');
+kvalid = [kleft, kright, kesc];
+
+
+% Sti Parameters
 vddot = .1; % In vd
 vdsphere = 8.2;
 ndots = 500;
@@ -22,6 +33,9 @@ spdsphere = 16; % in vd/s
 
 screenid = max(Screen('Screens'));
 FrameRate = Screen('NominalFrameRate', screenid);
+
+fperrun = secsperrun / FrameRate;
+resp = NaN(nRuns, fperrun);
 
 if screenid
     monitorh=30; %12;% in cm
@@ -40,27 +54,31 @@ white = WhiteIndex(screenid);
 
 [center(1), center(2)] = RectCenter(mrect);
 
-% calculate x and y for each dot
-angle = (rand(2, ndots).*2 -1) .* 180; % in 3d
-Updateangle = spdsphere /  FrameRate; % vd per frame
 
-angle_2d = NaN(size(angle));
-while ~KbCheck
-
-    angle_2d(2,:) = sind(angle(2,:)).* (vdsphere / 2);
-    angle_2d(1,:) = sind(angle(1,:)).*cosd(angle(2,:)).* (vdsphere / 2);
+for run = 1:nRuns
+    % calculate x and y for each dot
+    angle = (rand(2, ndots).*2 -1) .* 180; % in 3d
+    Updateangle = spdsphere /  FrameRate; % vd per frame
     
-    pix = ang2pix(angle_2d);
-
-    Screen('DrawDots', window, pix,ang2pix(vddot), white, center);
-    Screen('DrawDots', window, center, ang2pix(vdfix), white);
-
-    Screen('Flip', window);
-
-    angle(1,:) = angle(1,:) + Updateangle;
-
+    angle_2d = NaN(size(angle));
+    for flip = 1:fperrun
+        
+        angle_2d(2,:) = sind(angle(2,:)).* (vdsphere / 2);
+        angle_2d(1,:) = sind(angle(1,:)).*cosd(angle(2,:)).* (vdsphere / 2);
+        
+        pix = ang2pix(angle_2d);
+        
+        Screen('DrawDots', window, pix,ang2pix(vddot), white, center);
+        Screen('DrawDots', window, center, ang2pix(vdfix), white);
+        
+        Screen('Flip', window);
+        angle(1,:) = angle(1,:) + Updateangle;
+        resp(run, flip) = CheckResp;
+    end
+    
+    DrawFormattedText(window, ['end of block ', num2str(run), ', presse to start the next.'], 'center', 'center', black);
+    KbStrokeWait;
 end
-
 session_end;
 
     function pixels=ang2pix(ang)
@@ -72,5 +90,20 @@ session_end;
         ShowCursor;
         sca;
         return
+    end
+
+    function resp = CheckResp
+        [keyIsDown, ~, keyCode] = KbCheck;
+        knum = find(keyCode);
+        if keyIsDown && any(ismember(knum, kvalid)) && numel(knum) == 1
+            resp = knum;
+            if resp == kesc
+                session_end;return
+            end
+        else
+            resp = NaN;
+        end
+
+
     end
 end
