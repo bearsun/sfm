4d0eeeea1509e4142ae8cc80e1323a4c90aa2148
Liwei Sun <sunliwei.china@gmail.com>
2015-06-01 16:05:14 -0400

    "ana code"

---
+++ analysis/ana_pilot_sfm.m
@@ -0,0 +1,32 @@
+function durations = ana_pilot_sfm(filepath)
+
+%% function to analyze pilot data
+
+%% load in
+data = readtable(filepath);
+
+fperrun = 210*60;
+%% for loop...to scan
+durations = cell2table({});
+durations.Properties.VariableNames={'Run', 'Duration', 'Direction'}
+nrow = size(data , 1);
+init = data.flip(1);
+for k = 1:nrow
+    if strcmp(data.direction{k}, 'Escape')
+        disp(data(k,:));
+        break
+    end
+    
+    if ~strcmp(data.direction{k},data.direction{k+1}) || data.run(k) ~= data.run(k+1)
+        if data.run(k)~= data.run(k+1)
+            duration = fperrun - init;
+        else
+            duration = data.flip(k+1) - init;
+        end
+        durations = [durations ; cell2table({data.run(k), duration, data.direction{k}})]; %#ok<AGROW>
+        init = data.flip(k+1);
+    end
+    
+end
+durations.Properties.VariableNames = {'Run', 'Duration', 'Direction'};
+end
