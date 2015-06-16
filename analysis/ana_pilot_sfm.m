a0d1c79e6ffafdf44bc2859c7b7243cfb80de3ac
Liwei Sun <sunliwei.china@gmail.com>
2015-06-15 22:07:04 -0400

    ana after 1st subj

--- analysis/ana_pilot_sfm.m
+++ analysis/ana_pilot_sfm.m
@@ -3,17 +3,33 @@ function durations = ana_pilot_sfm(filepath)
 %% function to analyze pilot data
 
 %% load in
-data = readtable(filepath);
+data = readtable(filepath, 'Delimiter', '\t');
 
-fperrun = 210*60;
+fperrun = 180*60;
 %% for loop...to scan
 durations = cell2table({});
 nrow = size(data , 1);
 init = data.flip(1);
+flag_catch = 0;
 for k = 1:nrow
     if strcmp(data.direction{k}, 'Escape')
         disp(data(k,:));
         break
+    end
+    
+    if strcmp(data.direction{k+1}, 'CatchLeft')
+        flag_catch = 1;
+    elseif strcmp(data.direction{k+1}, 'CatchRight')
+        flag_catch = 2;
+    end
+
+    if strcmp(data.direction{k}, 'CatchEnd')
+        flag_catch = 0;
+        continue
+    end
+    
+    if flag_catch
+        continue
     end
     
     if ~strcmp(data.direction{k},data.direction{k+1}) || data.run(k) ~= data.run(k+1)
@@ -28,4 +44,27 @@ for k = 1:nrow
     
 end
 durations.Properties.VariableNames = {'Run', 'Duration', 'Direction'};
+
+%% baseline
+disp('Baseline: Left / Right / Down / All');
+baseline = durations(durations.Run<5,:);
+display_data(baseline);
+
+%% maintainance
+disp('Maintain: Left / Right / Down / All');
+maintain = durations(rem(durations.Run,2) == 1 & durations.Run>4,:);
+display_data(maintain);
+
+
+%% alternation
+disp('Alternation: Left / Right / Down / All');
+alter = durations(rem(durations.Run,2) == 0 & durations.Run>4,:);
+display_data(alter);
+
+    function display_data(d)
+        disp(mean(d.Duration(strcmp(d.Direction,'Left')))/60);
+        disp(mean(d.Duration(strcmp(d.Direction,'Right')))/60);
+        disp(mean(d.Duration(strcmp(d.Direction,'Down')))/60);
+        disp(mean(d.Duration/60));
+    end
 end
